bb00e1094b77b5e62e227c7a69effa5bb4b8d8df
steveminnikin <steveminnikin@hotmail.com>
2020-02-25 14:37:15 +0000

    algorithm working

--- Minnikin_Stephen_Hangman.py
+++ Minnikin_Stephen_Hangman.py
@@ -1,12 +1,14 @@
 import random
 import time
 
+guessed_characters = []
+
 #Welcome message
-print('Welcome to Steve Minnikin''s JHub Module 3 Submission, Let''s play a game of hangman')
+print('WOULD YOU LIKE TO PLAY A GAME........OF HANGMAN')
 
 time.sleep(2)
 
-print('I''m thinking of a word...')
+print('I''M THINKING OF A WORD...')
 
 time.sleep(2)
 
@@ -21,13 +23,44 @@ def generate_guess_word():
 
     return guess_word
 
-def hide_guess_word(guess_word: str):
-    for c in enumerate(guess_word):
-        c += '*'
-    return guess_word
-
 guess_word = generate_guess_word()
+
+def hide_guess_word(guess_word):
+    hidden_guess_word = list(guess_word)
+    for i in range(len(hidden_guess_word) -1):
+        hidden_guess_word[i] = '*'
+
+    s = ''
+    hidden_guess_word = s.join(hidden_guess_word)
+
+    return hidden_guess_word
 
 hidden_guess_word = hide_guess_word(guess_word)
 
 print(hidden_guess_word)
+print(guess_word)
+
+def test_guesses(guessed_characters, guess_word):
+    guess_word_list = list(guess_word)
+    print(guess_word_list)
+    for i in guessed_characters:
+        print(i)
+        for j in guess_word_list:
+            if i != j:
+                 j = '*'
+            print(i + ',' + j)
+        print(guess_word_list)
+
+    s = ''
+    guess_word = s.join(guess_word_list)
+
+    return
+
+
+
+guessed_characters.append(str(input('ENTER A GUESS: ')))
+
+test_guesses(guessed_characters, guess_word)
+
+#while guessed word != guess_word:
+    
